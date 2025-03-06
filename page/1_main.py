6aea4a15e4b54ccceccc3da0dd2787827d065716
paokungz <pikul874@gmal.com>
2025-03-06 23:33:40 +0700

    fix home

--- page/1_main.py
+++ page/1_main.py
@@ -1,25 +1,25 @@
 # 1_main.py
 import streamlit as st
-from page.page1 import app as page1_app  # เรียกใช้ฟังก์ชัน app() จาก page1
-from page.page2 import app as page2_app  # เรียกใช้ฟังก์ชัน app() จาก page2
-from page.page3 import app as page3_app  # เรียกใช้ฟังก์ชัน app() จาก page3
-from page.page4 import app as page4_app  # เรียกใช้ฟังก์ชัน app() จาก page3
-from page.page5 import app as page5_app  # เรียกใช้ฟังก์ชัน app() จาก page3
-
+from pathlib import Path
+from page1 import app1
+from page2 import app2
+from page3 import app3
+from page4 import app4
+from page5 import app5
 
 def main():
     # เมนูเลือกหน้า
     page = st.selectbox("เลือกหน้าที่ต้องการ", ["การเตรียมข้อมูล", "ทฤษฎีของอัลกอริทึมที่พัฒนา","การจัดการข้อมูล"," Random Forest และ Gradient Boosting","Neural Network"])
 
     if page == "การเตรียมข้อมูล":
-        page1_app()  # เรียกใช้ฟังก์ชัน app() จาก page1
+        app1()  # เรียกใช้ฟังก์ชัน app() จาก page1
     elif page == "ทฤษฎีของอัลกอริทึมที่พัฒนา":
-        page2_app()
+        app2()
     elif page == "การจัดการข้อมูล":
-        page3_app()
+        app3()
     elif page == " Random Forest และ Gradient Boosting":
-        page4_app()
+        app4()
     elif page == "Neural Network":
-        page5_app()
+        app5()
 if __name__ == "__main__":
     main()
